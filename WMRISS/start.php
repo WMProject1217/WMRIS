47fe3347d3e6604bd497eb326841fe694f9a37d8
WMProject1217 <72447462+WMProject1217@users.noreply.github.com>
2023-01-16 13:24:57 +0800

    Add files via upload

---
+++ WMRISS/start.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * run with command 
+ * php start.php start
+ */
+
+ini_set('display_errors', 'on');
+use Workerman\Worker;
+
+if(strpos(strtolower(PHP_OS), 'win') === 0)
+{
+    exit("start.php not support windows, please use start_for_win.bat\n");
+}
+
+// 检查扩展
+if(!extension_loaded('pcntl'))
+{
+    exit("Please install pcntl extension. See http://doc3.workerman.net/appendices/install-extension.html\n");
+}
+
+if(!extension_loaded('posix'))
+{
+    exit("Please install posix extension. See http://doc3.workerman.net/appendices/install-extension.html\n");
+}
+
+define('GLOBAL_START', 1);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+require_once '/project/start.php';
+
+Worker::runAll();
---
+++ WMRISS/start_WIN.bat
@@ -0,0 +1,7 @@
+CHCP 65001
+
+cd /d %~dp0
+:a
+php project\start.php
+goto a
+pause
